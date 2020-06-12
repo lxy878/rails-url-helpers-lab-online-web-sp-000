434851af37318a969275eebb76f3360527bc7f6c
Xiaoyi Liu <lxy878@gmail.com>
2020-06-12 11:34:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,10 +1,5 @@
 class AddActiveToStudents < ActiveRecord::Migration
   def change
-    create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
-
-      t.timestamps null: false
-    end
+    add_column :students, :active, :boolean, default: false
   end
 end
