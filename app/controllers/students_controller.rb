da7b8ef81b5f0c894c5f729335f92e5ab1d72705
Xiaoyi Liu <lxy878@gmail.com>
2020-06-12 12:28:14 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
     set_student
     @student.active = !@student.active
     @student.save
-    redirect student_path(@student.id)
+    redirect student_path(@student)
   end
 
   private
