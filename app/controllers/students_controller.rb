e7a5402b08a906e9d775d7f1137a9480c9373159
Xiaoyi Liu <lxy878@gmail.com>
2020-06-12 12:26:20 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def activate
-    set_student
+    @student= set_student
     @student.active = !@student.active
     @student.save
   end
